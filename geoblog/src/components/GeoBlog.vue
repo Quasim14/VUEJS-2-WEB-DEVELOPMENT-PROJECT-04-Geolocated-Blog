70f40044f9d488c371dc381286eb5a67837c9a18
quasim <nicomaxa@gmail.com>
2018-10-11 10:48:58 +0200

    Chapter06 Initial commit

---
+++ geoblog/src/components/GeoBlog.vue
@@ -0,0 +1,17 @@
+<template>
+
+  <div class="geo-blog">
+    <!-- More to come -->
+  </div>
+    
+</template>
+
+<script>
+    export default {
+        name: "GeoBlog"
+    }
+</script>
+
+<style scoped>
+
+</style>
