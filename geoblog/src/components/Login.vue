48b35af17d1e89746eb3e518169a87053ec49414
quasim <nicomaxa@gmail.com>
2018-10-13 10:07:28 +0200

    Chapter06 User in the store.

--- geoblog/src/components/Login.vue
+++ geoblog/src/components/Login.vue
@@ -35,6 +35,8 @@
 
     },
 
+
+
     mounted () {
       window.addEventListener('message', this.handleMessage)
     },
--- geoblog/src/store/index.js
+++ geoblog/src/store/index.js
@@ -37,9 +37,43 @@ const store = new Vuex.Store({
       state.user = user
     },
   },
+    async login ({ commit, dispatch }) {
+      try {
+        const user = await $fetch('user')
+        console.log('user', user)
+        commit('user', user)
+
+        if (user) {
+          // Redirect to the wanted route or home
+          router.replace(router.currentRoute.params.wantedRoute || { name: 'home' })
+
+          dispatch('logged-in')
+        }
+      } catch (e) {
+        console.warn(e)
+      }
+    },
+
+    logout ({ commit, dispatch }) {
+      commit('user', null)
+
+      $fetch('logout')
+
+      // If the route is private
+      // We go to the login screen
+      if (router.currentRoute.matched.some(r => r.meta.private)) {
+        router.replace({ name: 'login', params: {
+            wantedRoute: router.currentRoute.fullPath,
+          }})
+      }
+    },
+
+
   user () {
     return this.$store.getters.user
   },
+
+
 
 
 })
